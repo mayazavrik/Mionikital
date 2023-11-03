5d0ec6dc31137fc55b05222821b641404d31c425
mayazavrik <127432774+mayazavrik@users.noreply.github.com>
2023-11-03 15:02:19 +0300

    added little styles

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,44 +1,21 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
 
-// import ModalQuest from '../features/modalka/ModalQuest';
-// import NavBar from '../features/NavBar';
 import MainPage from '../features/main/MainPage';
-// import { useAppDispatch } from '../redux/store';
-import * as api from './api';
 import ErrorWindow from '../features/404/ErrorWindow';
-import LogReg from '../features/logreg/LogReg';
+// import LogReg from '../features/logreg/LogReg';
 import NavBar from '../features/navbar/NavBar';
-import type { User } from '../redux/type';
-// import { fetchUser } from '../features/logreg/api/api';
-// import type { Data } from '../features/logreg/types/type';
 
 function App(): JSX.Element {
-  // const dispatch = useAppDispatch();
-  // const user: User = useSelector((store: RootState): User => store.user.user);
-  // console.log(user);
-
-  // useEffect(() => {
-  //   api.fetchThemes().then((data) => dispatch({ type: 'themes/load', payload: data }));
-  //   api.fetchQuestions().then((data) => dispatch({ type: 'question/load', payload: data }));
-  //   fetchUser().then((data: Data) =>
-  //     data.message === 'success'
-  //       ? dispatch({ type: 'user/get', payload: { user: data.user } })
-  //       : console.log(data.message),
-  //   );
-  // }, []);
-  // console.log(user);
-
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<NavBar />}>
-          <Route path="/game" element={<MainPage />} />
-          <Route path="/" element={<LogReg />} />
+          <Route path="/main" element={<MainPage />} />
+          {/* <Route path="/" element={<LogReg />} /> */}
         </Route>
         <Route path="*" element={<ErrorWindow />} />
       </Routes>
